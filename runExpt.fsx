6374cba07d4bc30e3b2e7961bee04bd47c10c240
simra <robert.sim@live.com>
2016-04-30 20:14:53 -0700

    Running experiment Experiments\201604300814

--- runExpt.fsx
+++ runExpt.fsx
@@ -1,9 +1,9 @@
+open System
+open System.IO
 #load @"expUtil\expUtil.fsx"
 #load @"expUtil\runProc.fsx"
 open expUtil
 open runProc
-open System
-open System.IO
 
 // TODO: 
 // pass-through command-line args and include them in any commit message.
@@ -13,20 +13,23 @@ open System.IO
 // EDIT THESE FOR YOUR EXPERIMENT.
 let resultFile = "results.txt"
 let experimentCommand = sprintf "echo %%DATE%% %%TIME%% > %s" resultFile
+let expFolder = "Experiments"
 let verbosity=1
 
-let workingTag = DateTime.Now.ToString("yyyyMMddhhmm")
+if not (Directory.Exists(expFolder)) then Directory.CreateDirectory(expFolder)|>ignore
 
+let workingTag = sprintf "%s\%s" expFolder (DateTime.Now.ToString("yyyyMMddhhmm"))
       
 if repoIsReady() then 
     gitCommit(sprintf "Running experiment %s" workingTag)
-    gitTag workingTag (sprintf "Tagged experiment %s" workingTag)
+    gitTag workingTag (sprintf "Tagged experiment %s" workingTag) // necessary?
     gitSubmod(workingTag) // submodule or something else?
 
     runProc "cmd.exe" (sprintf "/c %s" experimentCommand) (Some workingTag) |> ignore
 
     let expectedResult=sprintf @"%s\%s" workingTag resultFile
     if File.Exists(expectedResult) then
+        let currentDir=Directory.GetCurrentDirectory()
         Directory.SetCurrentDirectory(workingTag)
         gitAdd(resultFile) |> ignore
         gitCommit(sprintf "Experiment %s results." workingTag) |> ignore
@@ -35,7 +38,7 @@ if repoIsReady() then
             File.ReadAllLines resultFile
             |> Seq.iter (printfn "%s")
         // Concat to global result file?
-        Directory.SetCurrentDirectory("..")
+        Directory.SetCurrentDirectory(currentDir)
 
 
 
